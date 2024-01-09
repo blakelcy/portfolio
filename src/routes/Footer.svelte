36c1cf3e82f3edec089bec8283439c219fb25528
Casey Blakely <casey.blakely@adventureware.com>
2024-01-09 16:50:06 -0500

    here goes nothing

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,24 @@
+<footer>
+	<p>Reprint 2024</p>
+	<div class=""></div>
+</footer>
+
+<style>
+	footer {
+		width: 100%;
+	}
+
+	footer > p {
+		font-size: 0.625rem;
+		text-align: right;
+		padding-right: 1rem;
+		margin: 0;
+		margin-bottom: 0.5rem;
+	}
+
+	footer > div {
+		width: 100%;
+		height: 24px;
+		background-color: black;
+	}
+</style>
